5d1b5734724e3ede0871f9db4703ce909f87c571
Jakub Kubrynski <jkubrynski@gmail.com>
2014-01-07 19:14:40 +0100

    Added option to override already existing files

--- files.sh
+++ files.sh
@@ -3,18 +3,28 @@
 script_dir=$PWD/configs
 
 function createlinks {
-    echo "Processing \"$1\""
     for file in $(ls -A $1); do
 	local current_file_path=$1/$file
-	local current_file_stripped=$(echo $current_file_path | sed 's|'$script_dir/'||g')
-	if [ ! -e ~/$current_file_stripped ]; then
-            echo "Creating ~/$current_file_stripped"
-            ln -s $current_file_path ~/$current_file_stripped
-        elif [ -d $1/$file ]; then
-	    echo "\"$current_file_stripped\" already exists - going deeper"
-	    createlinks $current_file_path
+	local current_file_stripped=~/$(echo $current_file_path | sed 's|'$script_dir/'||g')
+	if [ ! -e $current_file_stripped ]; then
+		createlink "$current_file_stripped" $current_file_path
+	elif [ -d $current_file_stripped ] && [ ! -h $current_file_stripped ]; then
+		printf "\"$current_file_stripped\" already exists - should I override it with symlink? [y/N] "
+		read -n 1 key
+		printf "\n"
+		if [[ $key = "y" ]]; then
+			rm -r $current_file_stripped
+			createlink "$current_file_stripped" $current_file_path
+		else
+			createlinks $current_file_path
+		fi
         fi
     done
+}
+
+function createlink {
+	echo "Creating $1"
+	ln -s $2 $1
 }
 
 case $1 in
